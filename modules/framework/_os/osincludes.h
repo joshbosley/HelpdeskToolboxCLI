094944d632e67f43850cc733fa2fac5ccba6805f
Josh Bosley <jbosley2@lssu.edu>
2015-10-02 08:06:02 -0400

    Small fix
    Small windows fixes

--- modules/framework/_os/osincludes.h
+++ modules/framework/_os/osincludes.h
@@ -1,22 +1,19 @@
 #ifndef HDTB_OSINCLUDES
 #define HDTB_OSINCLUDES
 
-
 #ifdef _WIN32
 
-
+    #include "windows.h"
 
 #elif __APPLE__
 
     // HDD Information
     #include <sys/statvfs.h>
     #include <sys/sysctl.h>
-#else
 
-
+\#else
 
 #endif
-
 
 #endif // HDTB_OSINCLUDES
 
